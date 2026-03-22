ec5d6a25ce785b008893062555db78a4242de63d
kelvin001-oteyo <oteyikelvin@gmail.com>
2026-03-22 16:28:50 +0300

    Update app references in Django settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,8 +24,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "payments",
-    "corsheaders",
+     "backend.payments",
+     "corsheaders",
+    
+    
+   
 ]
 
 # MIDDLEWARE
